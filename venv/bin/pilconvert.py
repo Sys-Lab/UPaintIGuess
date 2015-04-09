dab1b5487ad84e57d5eec1c6e656005037acb48d
excelle08 <suwei779@126.com>
2015-04-09 14:24:12 +0800

    Delete venv dir.Config venv customly instead.

--- venv/bin/pilconvert.py
+++
@@ -1,4 +0,0 @@
-#!/Users/Excelle/Documents/myprog/GuessWhatProj/venv/bin/python
-# EASY-INSTALL-SCRIPT: 'Pillow==2.8.1','pilconvert.py'
-__requires__ = 'Pillow==2.8.1'
-__import__('pkg_resources').run_script('Pillow==2.8.1', 'pilconvert.py')
